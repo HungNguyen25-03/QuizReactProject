ae3a253776b68d0bf405e56d93ada39931283f92
thienhung27hao@gmail.com <thienhung27hao@gmail.com>
2023-12-01 20:52:45 +0700

    1/12/2023

--- src/layout/ScreenResult/ScreenResult.js
+++ src/layout/ScreenResult/ScreenResult.js
@@ -2,25 +2,31 @@ import React, { useEffect } from "react";
 import Button from "../../components/Button";
 import "./ScreenResult.scss";
 import Score from "../../components/Score";
-import { useParams } from "react-router-dom";
 
 export default function ScreenResult() {
   let id = JSON.parse(localStorage.getItem("classId"));
   let data = JSON.parse(localStorage.getItem("result"));
-  const result = fetch(`https://server.nglearns.com/answer/${id}`, {
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify(data),
-  });
   console.log(data);
   useEffect(() => {
-    data = [];
-    localStorage.setItem("result", JSON.stringify(data));
+    fetch(`https://server.nglearns.com/answer/${id}`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
   }, []);
-  console.log("After delete" + data);
+  // useEffect(() => {
+  //   data = [];
+  //   localStorage.setItem("result", JSON.stringify(data));
+  // }, []);
 
   return (
     <div className="container">
